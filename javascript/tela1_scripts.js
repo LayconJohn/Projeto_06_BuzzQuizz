3b0b565cc78f8eebb238d0dfceba1cbfca1d1317
Laycon <layconjohn.sg@gmail.com>
2022-04-20 21:01:36 -0300

    Refatoração do JS

--- javascript/tela1_scripts.js
+++ javascript/tela1_scripts.js
@@ -1,33 +1,38 @@
-function renderizarMensagemTela1() {
-    document.querySelector(".tela1").innerHTML += `
-  <main>
-    <section class="meu-quizz">
-      <div class="quizz-vazio ">
-        <p>Você não criou nenhum quiz ainda :(</p>
-        <button>Criar Quizz</button>
-      </div>
-      <div class="quizz-completo oculto">
-        <div class="titulo">
-          <h3>Seus Quizzes</h3>
-          <ion-icon name="add-circle"></ion-icon>
-        </div>
-        <div class="quizzes">
-          <div class="quizz simpsons">
-            <p>
-              Acerte os personagens corretos dos Simpsons e prove seu amor!
-            </p>
-          </div>
-          <div class="quizz de-boas">
-            <p>
-              O quanto você é de boas?
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+const meuQuiz = document.querySelector(".meu-quizz");
+const todosQuizz = document.querySelector(".todos-quizz");
+const tela1 = document.querySelector(".tela1");
 
-    <section class="todos-quizz">
-      <h3>Todos os Quizzes</h3>
+function renderizarMensagemTela1() {
+  inserirTela1();
+  inserirMeuQuiz();
+  inserirTodosQuizz();
+}
+
+function inserirTela1 () {
+  document.querySelector(".tela1").innerHTML += `
+    <main>
+      <section class="meu-quizz">
+
+      </section>
+
+      <section class="todos-quizz">
+
+      </section>
+    </main>
+  `
+}
+
+function inserirMeuQuiz() {
+  document.querySelector(".meu-quizz").innerHTML = `
+    <div class="quizz-vazio ">
+      <p>Você não criou nenhum quiz ainda :(</p>
+      <button>Criar Quizz</button>
+    </div>
+    <div class="quizz-completo oculto">
+      <div class="titulo">
+        <h3>Seus Quizzes</h3>
+        <ion-icon name="add-circle"></ion-icon>
+      </div>
       <div class="quizzes">
         <div class="quizz simpsons">
           <p>
@@ -39,30 +44,48 @@ function renderizarMensagemTela1() {
             O quanto você é de boas?
           </p>
         </div>
-        <div class="quizz simpsons">
-          <p>
-            Acerte os personagens corretos dos Simpsons e prove seu amor!
-          </p>
-        </div>
-        <div class="quizz de-boas">
-          <p>
-            O quanto você é de boas?
-          </p>
-        </div>
-        <div class="quizz simpsons">
-          <p>
-            Acerte os personagens corretos dos Simpsons e prove seu amor!
-          </p>
-        </div>
-        <div class="quizz de-boas">
-          <p>
-            O quanto você é de boas?
-          </p>
-        </div>
       </div>
-    </section>
-  </main>
-    `
+    </div>
+  `
+}
+
+function inserirTodosQuizz() {
+  document.querySelector(".todos-quizz").innerHTML += `
+    <h3>Todos os Quizzes</h3>
+    <div class="quizzes">
+      <div class="quizz simpsons">
+        <p>
+          Acerte os personagens corretos dos Simpsons e prove seu amor!
+        </p>
+      </div>
+      <div class="quizz de-boas">
+        <p>
+          O quanto você é de boas?
+        </p>
+      </div>
+      <div class="quizz simpsons">
+        <p>
+          Acerte os personagens corretos dos Simpsons e prove seu amor!
+        </p>
+      </div>
+      <div class="quizz de-boas">
+        <p>
+          O quanto você é de boas?
+        </p>
+      </div>
+      <div class="quizz simpsons">
+        <p>
+          Acerte os personagens corretos dos Simpsons e prove seu amor!
+        </p>
+      </div>
+      <div class="quizz de-boas">
+        <p>
+          O quanto você é de boas?
+        </p>
+      </div>
+    </div>
+  `
 }
 
 renderizarMensagemTela1();
+
---
+++ javascript/tela2_scripts.js
@@ -0,0 +1,6 @@
+function inserir tela2 () {
+}
+
+function clicarBotao() {
+
+}
